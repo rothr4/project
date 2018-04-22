284da7234b4ed49e44a7702b972b8834995d7dfd
rothr4 <rothr4.rrrr@gmail.com>
2018-04-23 02:30:17 +0900

    Manual

--- WebHookLib/MattermostBot.cs
+++ WebHookLib/MattermostBot.cs
@@ -4,25 +4,21 @@ namespace WebHookLib
 {
     public class MattermostBot
     {
-        public string BotName { get; private set; }
-        public string WebHookId { get; private set; }
-        public string IconUrl { get; private set; }
         private IDataPostTemplete ObjTemp { get; set; }
 
-        public MattermostBot(string name, string webHookId)
+        public string WebHookId { get; private set; }
+        
+
+        public MattermostBot(string botName, string webHookId)
         {
-            this.BotName = name;
+            ObjTemp = new PostMessageDefault(botName);
             this.WebHookId = webHookId;
-            this.IconUrl = string.Empty;
-            ObjTemp = new PostMessageDefault(BotName);
         }
 
-        public MattermostBot(string name, string webHookId, string iconUrl)
+        public MattermostBot(string botName, string webHookId, string iconUrl)
         {
-            this.BotName = name;
+            ObjTemp = new PostMessageAddIcon(botName, iconUrl);
             this.WebHookId = webHookId;
-            this.IconUrl = iconUrl;
-            ObjTemp = new PostMessageAddIcon(BotName, iconUrl);
         }
 
         public object GetTest(string message)
